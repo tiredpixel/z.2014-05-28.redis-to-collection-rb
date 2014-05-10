a8f600e771756c69b0ca73cad39a72c76b499fc6
tiredpixel <tp@tiredpixel.com>
2014-05-10 20:04:54 +0100

    Add Loadling , for loading Redis from a collection.
    Add `Loadling`, which should be instantiated with a Redis connection.
    `Loadling` is the opposite of `Dumpling`. `Loadling#load` loads a
    collection into Redis. Add convenience top-level `::load`, wrapping a
    `Loadling`.

--- lib/redis-to-collection.rb
+++ lib/redis-to-collection.rb
@@ -1,12 +1,17 @@
 require_relative 'redis-to-collection/version'
 require_relative 'redis-to-collection/constants'
 require_relative 'redis-to-collection/dumpling'
+require_relative 'redis-to-collection/loadling'
 
 
 module RedisToCollection
   
   def self.dump(redis, pattern = nil)
     RedisToCollection::Dumpling.new(redis, pattern).dump
+  end
+  
+  def self.load(redis)
+    RedisToCollection::Loadling.new(redis).load
   end
   
 end
